bf8e53abbcaab1fe0fa1136673aa69b96f537738
KlnSdr <monsieurk1209@gmail.com>
2023-10-18 20:29:07 +0200

    DOBBY-41: http method annotations can only be set for methods

--- src/dobby/annotations/Delete.java
+++ src/dobby/annotations/Delete.java
@@ -1,11 +1,10 @@
 package dobby.annotations;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.*;
 
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Delete {
     String value();
 }
--- src/dobby/annotations/Put.java
+++ src/dobby/annotations/Put.java
@@ -1,14 +1,13 @@
 package dobby.annotations;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.*;
 
 /**
  * Annotation for PUT requests
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Put {
     String value();
 }
