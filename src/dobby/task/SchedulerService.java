2212a7c16a8e7f8e671b9ee129d27becbbae70e1
KlnSdr <monsieurk1209@gmail.com>
2023-12-23 17:33:34 +0100

    DOBBY-47: add option to disable all schedulers

--- src/dobby/task/SchedulerService.java
+++ src/dobby/task/SchedulerService.java
@@ -1,5 +1,8 @@
 package dobby.task;
 
+import dobby.util.Config;
+import dobby.util.logging.Logger;
+
 import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ScheduledExecutorService;
@@ -8,6 +11,8 @@ import java.util.concurrent.TimeUnit;
 public class SchedulerService {
     private static SchedulerService instance;
     private final ArrayList<ScheduledExecutorService> schedulers = new ArrayList<>();
+    private final boolean isDisabled;
+    private static final Logger LOGGER = new Logger(SchedulerService.class);
 
     public static SchedulerService getInstance() {
         if (instance == null) {
@@ -16,7 +21,15 @@ public class SchedulerService {
         return instance;
     }
 
+    private SchedulerService() {
+        isDisabled = Config.getInstance().getBoolean("dobby.scheduler.disabled", false);
+    }
+
     public void addRepeating(Runnable task, int interval, TimeUnit unit) {
+        if (isDisabled) {
+            LOGGER.info("Scheduler is disabled, not scheduling task");
+            return;
+        }
         final ScheduledExecutorService scheduler = java.util.concurrent.Executors.newScheduledThreadPool(1);
         scheduler.scheduleAtFixedRate(task, 0, interval, unit);
         schedulers.add(scheduler);
