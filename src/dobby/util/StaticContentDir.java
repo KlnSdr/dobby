6950ac7072b8d65a4171f9d9f4117f9995076cb3
KlnSdr <monsieurk1209@gmail.com>
2024-02-19 23:54:27 +0100

    add method to add another static content dir to the config easily

---
+++ src/dobby/util/StaticContentDir.java
@@ -0,0 +1,17 @@
+package dobby.util;
+
+public class StaticContentDir {
+    public static void appendToContentDir(Class<?> baseClass, String location) {
+        final Config config = Config.getInstance();
+        final String staticContentDirKey = "dobby.staticContent.directory";
+        String currentConfig = config.getString(staticContentDirKey, "");
+
+        if (!currentConfig.isEmpty()) {
+            currentConfig += ",";
+        }
+
+        final String baseClassName = baseClass.getCanonicalName();
+
+        config.setString(staticContentDirKey, currentConfig + baseClassName + "\\" + location);
+    }
+}
