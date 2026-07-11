519505d609b5e2b6a049e13b8534a60b45be6772
KlnSdr <monsieurk1209@gmail.com>
2026-07-11 19:36:59 +0200

    serializer can generate type info in json format

--- src/dobby/io/dto/ISerializer.java
+++ src/dobby/io/dto/ISerializer.java
@@ -4,4 +4,5 @@ import dobby.util.json.NewJson;
 
 public interface ISerializer {
     NewJson serialize(Object object);
+    NewJson getJsonType(Object object);
 }
--- src/dobby/io/dto/Serializer.java
+++ src/dobby/io/dto/Serializer.java
@@ -5,7 +5,12 @@ import common.logger.Logger;
 import dobby.util.json.NewJson;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RegisterFor(ISerializer.class)
 public class Serializer implements ISerializer {
@@ -34,10 +39,41 @@ public class Serializer implements ISerializer {
                     case Float f -> json.setFloat(field.getName(), f);
                     case Boolean b -> json.setBoolean(field.getName(), b);
                     case List<?> l -> serializeList(field.getName(), l, json);
+                    case UUID u -> json.setString(field.getName(), u.toString());
                     default -> json.setJson(field.getName(), serialize(value));
                 }
             } catch (IllegalAccessException e) {
                 LOGGER.error("Error occurred while serializing object");
+                LOGGER.trace(e);
+            }
+        }
+
+        return json;
+    }
+
+    @Override
+    public NewJson getJsonType(Object object) {
+        if (object == null) {
+            return null;
+        }
+
+        final Field[] fields = object.getClass().getDeclaredFields();
+        final NewJson json = new NewJson();
+        for (Field field : fields) {
+            try {
+                field.setAccessible(true);
+
+                switch (field.getType().getSimpleName()) {
+                    case "String" -> json.setString(field.getName(), String.class.getSimpleName());
+                    case "Integer" -> json.setString(field.getName(), Integer.class.getSimpleName());
+                    case "Double" -> json.setString(field.getName(), Double.class.getSimpleName());
+                    case "Float" -> json.setString(field.getName(), Float.class.getSimpleName());
+                    case "Boolean" -> json.setString(field.getName(), Boolean.class.getSimpleName());
+                    case "List" -> json.setString(field.getName(), describeListType(field));
+                    default -> json.setString(field.getName(), field.getType().getSimpleName());
+                }
+            } catch (Exception e) {
+                LOGGER.error("Error occurred while getting JSON type for field " + field.getName());
                 LOGGER.trace(e);
             }
         }
@@ -56,6 +92,7 @@ public class Serializer implements ISerializer {
             case Double ignored -> json.setList(key, list.stream().map(Double.class::cast).map(o -> (Object) o).toList());
             case Float ignored -> json.setList(key, list.stream().map(Float.class::cast).map(o -> (Object) o).toList());
             case Boolean ignored -> json.setList(key, list.stream().map(Boolean.class::cast).map(o -> (Object) o).toList());
+            case UUID ignored -> json.setList(key, list.stream().map(UUID.class::cast).map(UUID::toString).map(o -> (Object) o).toList());
             default -> {
                 final List<Object> serializedList = list.stream()
                         .map(this::serialize)
@@ -64,5 +101,34 @@ public class Serializer implements ISerializer {
                 json.setList(key, serializedList);
             }
         }
+    }
+
+    private String describeListType(Field field) {
+        final Type genericType = field.getGenericType();
+        return describeType(genericType);
+    }
+
+    private String describeType(Type type) {
+        if (type instanceof ParameterizedType parameterizedType) {
+            final Type raw = parameterizedType.getRawType();
+            final String rawName = (raw instanceof Class<?> rawClass) ? rawClass.getSimpleName() : raw.getTypeName();
+
+            final Type[] typeArgs = parameterizedType.getActualTypeArguments();
+            if (typeArgs.length == 0) {
+                return rawName;
+            }
+
+            final String argsDescription = Arrays.stream(typeArgs)
+                    .map(this::describeType)
+                    .collect(Collectors.joining(", "));
+
+            return rawName + "<" + argsDescription + ">";
+        }
+
+        if (type instanceof Class<?> clazz) {
+            return clazz.getSimpleName();
+        }
+
+        return type.getTypeName();
     }
 }
